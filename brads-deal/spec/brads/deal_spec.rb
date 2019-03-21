544032d75e468bd068512599673236a0375d3142
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-21 15:59:01 +0000

    delete brads deals

--- brads-deal/spec/brads/deal_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Brads::Deal do
-  it "has a version number" do
-    expect(Brads::Deal::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
